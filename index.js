75a5f0c323551ab8fafb3d0f855b2a2a5e2f2fe0
WBRK-dev <koertjrweberp@gmail.com>
2023-05-18 16:22:05 +0200

    Oauth test

--- index.js
+++ index.js
@@ -4,10 +4,45 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.send(`Received a GET HTTP method ${process.env.client_id}`);
+let MAL_ACCESSTOKEN_URL = "https://myanimelist.net/v1/oauth2/token";
+let CODE_VERIFIER = "this-is-bs-and-i-hate-this-part-so-just-make-something-really-massive";
+
+app.get('/accesstoken/authorize', async (req, res) => {
+    let code = req.query.code;
+
+	console.log(`AUTH - ${code}`);
+
+	let url = MAL_ACCESSTOKEN_URL;
+
+	let body = `client_id=${process.env.client_id}&client_secret=${process.env.client_secret}&grant_type=authorization_code&code_verifier=${CODE_VERIFIER}&code=${code}`;
+
+	let response = await fetch(url, {
+		headers: {
+		  'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+		},
+		method: 'POST',
+		body,
+	});
+
+	let json = await response.json();
+
+    return res.send(json);
+});
+
+app.get('/accesstoken/refresh', async (req, res) => {
+    let code = req.query.code;
+
+	let response = await fetch(url, {
+		headers: {
+		  'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+		},
+		method: 'POST',
+		body,
+	});
+
+    return res.send({"status": 200});
 });
 
 app.listen(8088, () =>
-  console.log(`Example app listening on port 8088!`),
+    console.log(`Anime API listening on port 8088!`),
 );
