e0ffe2863d7896d41b10d59fc9ec63b90873fbba
WBRK-dev <koertjrweberp@gmail.com>
2023-05-20 16:28:53 +0200

    Vecel update 7

--- api/index.js
+++ api/index.js
@@ -1,16 +1,12 @@
 const app = require('express')();
 
-app.get('/api', (req, res) => {
-	const path = `/api/item/test`;
-	res.setHeader('Content-Type', 'text/html');
-	res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-	res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+app.get('/authorize', (req, res) => {
+	res.send({"yes": true})
 });
 
 
-app.get('/api/item/:slug', (req, res) => {
-	const { slug } = req.params;
-	res.end(`Item: ${slug}`);
-});
+
+
+
 
 module.exports = app;
