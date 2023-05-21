9b16543eebabbb8be35da200086559d4b41de1d6
WBRK-dev <koertjrweberp@gmail.com>
2023-05-21 15:44:55 +0200

    Prod test 9

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const crypto = require("crypto");
 
 let MAL_ACCESSTOKEN_URL = "https://myanimelist.net/v1/oauth2/token";
 let CODE_VERIFIER = "this-is-bs-and-i-hate-this-part-so-just-make-something-really-massive";
+let tokens = [];
 
 app.get('/api/authorize', async (req, res) => {
 	res.setHeader('Access-Control-Allow-Origin', 'https://wbrk-anime.pages.dev');
@@ -22,6 +23,12 @@ app.get('/api/authorize', async (req, res) => {
 	if (json.access_token !== null && json.access_token !== undefined) {
 		let sessionid = crypto.randomUUID();
 
+		tokens.push({
+			sessionid: sessionid,
+			access: json.access_token,
+			refresh: json.refresh_token
+		});
+
 		res.cookie("sessionid", sessionid, {httpOnly: true, secure: true, sameSite: "none"});
 
 		return res.send({"succesfull": true});
@@ -30,7 +37,39 @@ app.get('/api/authorize', async (req, res) => {
 	}
 });
 
+function refreshToken() {
 
+}
+
+app.get('/api/list/get', async (req, res) => {
+	let sessionid = req.cookies.sessionid;
+	let accesstoken = "";
+
+	for (let i = 0; i < tokens.length; i++) {if (tokens[i].sessionid === sessionid) {accesstoken = tokens[i].access;break;}}
+
+	let response = await fetch("", {
+		headers: {
+			"Authorization": `Bearer ${accesstoken}`
+		}
+	});
+
+
+	res.send(await response.json());
+});
+
+
+
+
+
+
+
+
+
+
+
+app.get('/api/gettokens', (req, res) => {
+	res.send(tokens);
+});
 
 // app.listen(8088, () =>
 //     console.log(`Anime API listening on port 8088!`),
