8245881c877f429d85a67d59006bac90fae5e836
WBRK-dev <koertjrweberp@gmail.com>
2023-05-22 20:31:01 +0200

    Fixed accesstoken issue

--- api/user/info.js
+++ api/user/info.js
@@ -1,7 +1,7 @@
 module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://wbrk-anime.pages.dev');
 	res.setHeader('Access-Control-Allow-Credentials', true);
-	let accesstoken = req.tokens.find(req.cookies.sessionid)
+	let accesstoken = req.tokens.find(req.cookies.sessionid).accesstoken
 	let response = await fetch("https://api.myanimelist.net/v2/users/@me?fields=anime_statistics", {
 		headers: {
 			"Authorization": `Bearer ${accesstoken}`
