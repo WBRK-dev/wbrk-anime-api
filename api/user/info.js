1f4ff734b6a84e04130e19963b4f627aca3079e9
WBRK-dev <koertjrweberp@gmail.com>
2023-05-22 09:33:25 +0200

    t

--- api/user/info.js
+++ api/user/info.js
@@ -5,15 +5,17 @@ module.exports = async (req, res) => {
 	res.setHeader('Access-Control-Allow-Credentials', true);
 	let sessionid = req.cookies.sessionid;
 	let accesstoken = "";
-	let tempTokens = tokens.get();
-
-	for (let i = 0; i < tempTokens.length; i++) {if (tempTokens[i].sessionid === sessionid) {accesstoken = tempTokens[i].access;break;}}
-
-	let response = await fetch("https://api.myanimelist.net/v2/users/@me?fields=anime_statistics", {
-		headers: {
-			"Authorization": `Bearer ${accesstoken}`
-		}
-	});
-
-	res.send({res: await response.json(), access: accesstoken, tempTokens: tempTokens});
+	setTimeout(async () => {
+		let tempTokens = tokens.get();
+	
+		for (let i = 0; i < tempTokens.length; i++) {if (tempTokens[i].sessionid === sessionid) {accesstoken = tempTokens[i].access;break;}}
+	
+		let response = await fetch("https://api.myanimelist.net/v2/users/@me?fields=anime_statistics", {
+			headers: {
+				"Authorization": `Bearer ${accesstoken}`
+			}
+		});
+	
+		res.send({res: await response.json(), access: accesstoken, tempTokens: tempTokens});
+	}, 1000)
 }
