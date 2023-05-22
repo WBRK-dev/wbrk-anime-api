ed298daec5729fb5876c9620e3cf0e7c19e2584c
WORMSS <wormssmail@gmail.com>
2023-05-22 20:27:48 +0100

    Working

--- api/index.js
+++ api/index.js
@@ -1,21 +1,19 @@
 const app = require('express')();
 const cookieParser = require("cookie-parser");
-const tokens = require("./vars");
 
 app.use(cookieParser());
-app.use((req, res, next) => {
-	req.tokens = require("./vars");
-	next();
-})
+app.use(require('./assignTokensObj'));
 
 let MAL_ACCESSTOKEN_URL = "https://myanimelist.net/v1/oauth2/token";
 let CODE_VERIFIER = "this-is-bs-and-i-hate-this-part-so-just-make-something-really-massive";
 global.MAL_ACCESSTOKEN_URL = MAL_ACCESSTOKEN_URL;
 global.CODE_VERIFIER = CODE_VERIFIER;
 
+const getAccessToken = require('./getAccessToken');
+app.get('/api/grant/', (req, res) => res.redirect(`https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id=${process.env.client_id}&code_challenge=${CODE_VERIFIER}`))
 app.get('/api/authorize', require("./auth/authorize"));
-app.get('/api/list/get', require("./list/get"));
-app.get('/api/user/info', require("./user/info"));
+app.get('/api/list/get', getAccessToken, require("./list/get"));
+app.get('/api/user/info', getAccessToken, require("./user/info"));
 
 
 
@@ -38,14 +36,15 @@ app.get('/api/test', async (req, res) => {
 	res.send({success: true});
 })
 
-app.get('/api/test/gettokens', (req, res) => {
+app.get('/api/test/gettokens', getAccessToken, (req, res) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Credentials', true);
-	res.send({tokens_get: tokens.get(), tokensreq: req.accesstoken});
+	res.send({tokens_get: req.tokens.get(), tokensreq: req.accesstoken});
 });
 
 // app.listen(8088, () =>
 //     console.log(`Anime API listening on port 8088!`),
 // );
 
+app.all((req, res) => res.writeHead(404).end('unknown'))
 module.exports = app;
