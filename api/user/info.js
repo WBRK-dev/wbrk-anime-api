7c76ae563ef17ca3c673b36fc078b4cdcef415d5
WBRK-dev <koertjrweberp@gmail.com>
2023-05-22 10:39:27 +0200

    t

--- api/user/info.js
+++ api/user/info.js
@@ -4,6 +4,7 @@ module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://wbrk-anime.pages.dev');
 	res.setHeader('Access-Control-Allow-Credentials', true);
 	let accesstoken = req.accesstoken;
+	console.log(accesstoken);
 	let response = await fetch("https://api.myanimelist.net/v2/users/@me?fields=anime_statistics", {
 		headers: {
 			"Authorization": `Bearer ${accesstoken}`
